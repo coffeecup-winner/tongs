e9d3a071e202cdc0774537003ec0f776d81bdddb
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2016-10-19 22:44:05 +0200

    Add file data source

---
+++ src/Tongs/DataSource/FileDataSource.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tongs.DataSource
+{
+    public class FileDataSource : IEnumerable<File>
+    {
+        public const string AnyFilePattern = "*";
+
+        private readonly IReadOnlyCollection<File> files;
+
+        private FileDataSource(IEnumerable<File> files)
+        {
+            this.files = files.ToArray();
+        }
+
+        public IEnumerator<File> GetEnumerator()
+        {
+            return files.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public static FileDataSource CreateFromDirectory(string directory, string pattern = AnyFilePattern, bool recursively = true)
+        {
+            return new FileDataSource(Directory.EnumerateFiles(Path.GetFullPath(directory), pattern, recursively ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                .Select(p => new File(p)));
+        }
+    }
+
+    public class File
+    {
+        private readonly string path;
+
+        internal File(string path)
+        {
+            this.path = path;
+        }
+
+        public override string ToString()
+        {
+            return path;
+        }
+    }
+}
---
+++ src/Tongs/Get.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Tongs.DataSource;
+
+namespace Tongs
+{
+    public static class Get
+    {
+        public static IEnumerable<File> Files(string directory, string pattern = FileDataSource.AnyFilePattern, bool recursively = true)
+        {
+            return FileDataSource.CreateFromDirectory(directory, pattern, recursively);
+        }
+    }
+}
